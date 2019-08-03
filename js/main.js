2d5db2121ead6972027329c45afc0f4ee7dbd872
4l3j4ndr0cruz1233 <hcruz0716@gmail.com>
2019-08-02 22:29:38 -0500

    NO avance mucho :'c

--- js/main.js
+++ js/main.js
@@ -1,59 +1,33 @@
-if ((3 === '3') || ( 3 === 3 )) {
-  console.log('Si se va a ejecutar');
-}
+var name = "rotceH"
+var index = 5
+var normalicedName = ''
 
-//
-/*
+do{
+  normalicedName = normalicedName + name[index]
+    index-- 
+} while (index >= 0)
 
-var height = 100;
+console.log (normalicedName)
 
-if ((height < 150) && (height >= 100)) {
-  console.log('Pitufo');
-} else if ((height > 170) && (height < 200)) {
-  console.log('jirafa');
-} else if ((height >= 150) && (height <= 170)) {
-  console.log('ok');
-} else {
-  console.log('No seas payaso');
-}
+
+/*  
+L    I   
+6 - (5 + 1) = 0 - H
+6 - (4 + 1) = 1 - e
+6 - (3 + 1) = 2 - c
+6 - (2 + 1) = 3 - t
+6 - (1 + 1) = 4 - o
+6 - (0 + 1) = 5 - r
 */
-// Lengt (property) return the lengt of string "Cantidad de caracteres que entrega"
 
-var number = 1 
+var world = 'Alejandro'
+var arrayOfVowels = ['a', 'e', 'i', 'o', 'u']
 
-while( number === 1){
-  console.log('si')
-  number++
+for(let i = 0; i < world.length; i++ ){
+  console.log(i)
 }
 
-//
+/*if ((world[0]) === arrayOfVowels){
+  console.log('')
+}*/
 
-var name = 'David'
-var lastName = 'Cermeño Moranchel'
-var birthDate = '14-11-1996'
-var gender = 'Hombre'
-var birthCity = 'DF'
-
-var abbreviationName = (
- lastName.substring(0, 2) +
- lastName.charAt(lastName.indexOf(' ') + 1) +
- name.charAt(0)
-)
-var year = birthDate.slice(-2)
-var month = birthDate.slice(3, 5)
-var day = birthDate.slice(0, 2)
-var abbreviationGender = gender.charAt(0)
-
-
-var curp = `${abbreviationName}${year}${month}${day}${abbreviationGender}${birthCity}`
-
-
-//New exercise 
-var array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-var i = 0
-
-
-while(i < array.length){
-  console.log(array[i] ** 2)
-  i++
-}
