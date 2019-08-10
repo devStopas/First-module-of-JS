5b9402074b5d690acd60246b4d2a24b8cfb537d1
4l3j4ndr0cruz1233 <hcruz0716@gmail.com>
2019-08-09 20:57:27 -0500

    Exercise 2

--- js/main.js
+++ js/main.js
@@ -1,24 +1,86 @@
-//let numbers = [1,2,3,4,5,6,7,8,9,]
+/* 
+Carro 
 
-//numbers.map((numbers) => console.log(numbers % 2 === 0))
+color : rojo 
+capacidad : 5 personas
+puertas : 
 
-//numbers.forEach(item => {
-//  (item % 2 === 0) ? console.log('Es par') : console.log('No es par')
-//})
+*/
 
-let phrase = ['avion','casa','Arturo','nop','lol','perro',]
-let result = phrase.filter((phrase) => phrase[0] == 'a' || phrase[0] == 'A')
+//var carObj ={
+//    color: 'red',
+//    capacity: '5 people',
+//    brand: 'Ford',
+//    model: '2001',
+//    doors: '4',
+//    type: 'automatic'
+//} 
+//
+//for (let auto in carObj){
+//    console.table()
+//}
+//
+//var coursesArray = [
+//    {
+//        'name' : 'History',
+//        'courseGrade' : 8.1
+//    },
+//    {
+//        'name' : 'Sciences',
+//        'courseGrade' : 10
+//    },
+//    {
+//        'name' : 'Geography',
+//        'courseGrade' : 9.1
+//    },
+//    {
+//        'name' : 'Math',
+//        'courseGrade' : 7.5
+//    },
+//    {
+//        'name' : 'Languages',
+//        'courseGrade' : 9.6
+//    },
+//]
+//var result = coursesArray.reduce((total, {grade} ) => total + grade, 0) / coursesArray.length
 
-console.log(result)
+var caesarShift = function(str, amount) {
 
-//Retornar el total de cada uno de los elementos del {array} al cuadrado
-let numbers = [1,2,3,4,5]
+	// Wrap the amount
+	if (amount < 0)
+		return caesarShift(str, amount + 26);
 
-// inline
-[2,2,3,4,5].reduce((initial, current) =>  initial + current**2, 0)
+	// Make an output variable
+	var output = '';
 
-// explicit
-[2,2,3,4,5].reduce((initial, current) => {
-    console.log(initial, current**2)
-    return initial + current**2
-}, 0)   
+	// Go through each character
+	for (var i = 0; i < str.length; i ++) {
+
+		// Get the character we'll be appending
+		var c = str[i];
+
+		// If it's a letter...
+		if (c.match(/[a-z]/i)) {
+
+			// Get its code
+			var code = str.charCodeAt(i);
+
+			// Uppercase letters
+			if ((code >= 65) && (code <= 90))
+				c = String.fromCharCode(((code - 65 + amount) % 26) + 65);
+
+			// Lowercase letters
+			else if ((code >= 97) && (code <= 122))
+				c = String.fromCharCode(((code - 97 + amount) % 26) + 97);
+
+		}
+
+		// Append
+		output += c;
+
+	}
+
+	// All done!
+	return output;
+
+};
