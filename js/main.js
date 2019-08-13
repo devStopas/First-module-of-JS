b131c00b52c5e13a00f75c50558419177856e52f
4l3j4ndr0cruz1233 <hcruz0716@gmail.com>
2019-08-12 19:39:12 -0500

    No termine

--- js/main.js
+++ js/main.js
@@ -1,86 +1,51 @@
-/* 
-Carro 
+var person = {
+    name: 'Thelma',
+    birthDate: '1996-14-11',
+    height: {
+      unit: 'cm',
+      value: '170'
+    },
+    weight: {
+      unit: 'kg',
+      value: '70'
+    },
+    curp: '',
+    get getHeight() {
+      return `${this.height.value} ${this.height.unit}`
+    },
+    set setCURP(curp) {
+      this.curp = curp.toUpperCase()
+    }
+  }
+  person.getHeight
+  person.setCURP = 'hsdahjkdsfhjkds'
 
-color : rojo 
-capacidad : 5 personas
-puertas : 
+//Mi codigo no funcioal
 
-*/
+var Phone = {
+    brand : 'Samsung',
+    model : 'S9',
+    color : 'Res',
+    price : '$20,000',
+    memoryRam: '16 Gb',
+    memoryRom: '32 Gb',
+    camera: '56 mpx',
+    processor: '350MHz',
+    ziseScreen: '900px',
+    batery: {
+        mAh: 4000,
 
-//var carObj ={
-//    color: 'red',
-//    capacity: '5 people',
-//    brand: 'Ford',
-//    model: '2001',
-//    doors: '4',
-//    type: 'automatic'
-//} 
-//
-//for (let auto in carObj){
-//    console.table()
-//}
-//
-//var coursesArray = [
-//    {
-//        'name' : 'History',
-//        'courseGrade' : 8.1
-//    },
-//    {
-//        'name' : 'Sciences',
-//        'courseGrade' : 10
-//    },
-//    {
-//        'name' : 'Geography',
-//        'courseGrade' : 9.1
-//    },
-//    {
-//        'name' : 'Math',
-//        'courseGrade' : 7.5
-//    },
-//    {
-//        'name' : 'Languages',
-//        'courseGrade' : 9.6
-//    },
-//]
-//var result = coursesArray.reduce((total, {grade} ) => total + grade, 0) / coursesArray.length
+    }
+}
 
-var caesarShift = function(str, amount) {
+//Funcon Constructora  //capitalize
 
-	// Wrap the amount
-	if (amount < 0)
-		return caesarShift(str, amount + 26);
+// Constructor function
+function Dog(breed, name, size) {
+    this.breed = breed
+    this.name = name
+    this.size = size
+   }
+   var firstDog = new Dog('Dash hound', 'Sr. Salchicha', 'S')
 
-	// Make an output variable
-	var output = '';
-
-	// Go through each character
-	for (var i = 0; i < str.length; i ++) {
-
-		// Get the character we'll be appending
-		var c = str[i];
-
-		// If it's a letter...
-		if (c.match(/[a-z]/i)) {
-
-			// Get its code
-			var code = str.charCodeAt(i);
-
-			// Uppercase letters
-			if ((code >= 65) && (code <= 90))
-				c = String.fromCharCode(((code - 65 + amount) % 26) + 65);
-
-			// Lowercase letters
-			else if ((code >= 97) && (code <= 122))
-				c = String.fromCharCode(((code - 97 + amount) % 26) + 97);
-
-		}
-
-		// Append
-		output += c;
-
-	}
-
-	// All done!
-	return output;
-
-};
+function beer( )
