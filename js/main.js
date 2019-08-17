4e2a2a740e7ec6c889e119ad12790f3ca7a45dcc
4l3j4ndr0cruz1233 <hcruz0716@gmail.com>
2019-08-16 19:33:10 -0500

    class-completed

--- js/main.js
+++ js/main.js
@@ -1,21 +1,29 @@
-// document
-//     .getElementsByTagName('button')[0]
-//     .addEventListener('click', function({target}) {
-//         // target.style.backgroundColor === 'peru' 
-//         // ? target.style.backgroundColor = 'white'
-//         // : target.style.backgroundColor = 'peru'
-//         console.log(e.target.style);
-//     })
 
-for (let i = 0; i <= i; i++){
-  document
-  .getElementsByClassName('someclass')[i]
-  .addEventListener('mouseover', (e) => {
-       e.target.style.backgroundColor = 'red' 
-       console.log(e)
-  })
-  
+// document.getElementById('first').addEventListener('click', function({target}){
+//   console.log('padre', target);
+// })
+// document.getElementById('second').addEventListener('click', function({target}){
+//   console.log('hijo', target);
+// })
+// document.getElementById('third').addEventListener('click', function(event){
+//   console.log('nieto');
+//   event.stopPropagation()
+// })
+
+// //bobling del negro al verde
+// //captifing del verde al negro C:
+
+
+var xhttp = new XMLHttpRequest()
+xhttp.onreadystatechange = function () {
+    // console.log(this.readyState, this.status)
+    if (this.readyState === 4 && this.status === 200) {
+        console.log(this.response);
+        document.write(this.response)
+    } else if (this.readyState === 4 && this.status === 404) {
+        console.log('No encontre nada perro');
+    }
 }
-
-const changeColor({style}) => styler.color === 'red' ? 'blue' : 'red'
+xhttp.open("GET", "data.json")
+xhttp.send()
 
